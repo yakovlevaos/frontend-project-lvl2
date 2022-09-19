5fc66326e3a1a4b46e899c8de805e7a30b76e9c8
Olga <yurkovaos@gmail.com>
2022-09-19 11:51:15 +0700

    .

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>',  'output format');
 program.parse();
+
 
