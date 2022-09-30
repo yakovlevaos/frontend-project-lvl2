75f69edde23ec50e8ce05295b03e407937f5cfd0
Olga <yurkovaos@gmail.com>
2022-09-30 16:03:54 +0700

    Step_4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>',  'output format')
+  .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
-});
+  });
 program.parse();
-
-
